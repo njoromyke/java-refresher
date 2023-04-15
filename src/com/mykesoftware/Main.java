05b01e9b846865939040be88fb7fb57443e55c79
njoromyke <mykenjoroge01@gmail.com>
2023-04-15 15:49:23 +0300

    Strings

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -2,13 +2,14 @@ package com.mykesoftware;
 
 import java.awt.*;
 import java.util.Date;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        point1.x = 3;
-        System.out.println(point2);
+        String message = "Hello World" + "John";
 
+        System.out.println( message.replace("H","*"));
+        System.out.println(message.trim());
     }
+
 }
