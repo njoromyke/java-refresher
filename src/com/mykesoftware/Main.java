aab998471841bd37adc46cafc6b74b8fc52bcb49
njoromyke <mykenjoroge01@gmail.com>
2023-04-15 15:53:04 +0300

    Escape sequences

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -6,10 +6,12 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello World" + "John";
 
-        System.out.println( message.replace("H","*"));
-        System.out.println(message.trim());
+        String message = "Hello \"Mike\" "; // double quote
+        String message2 = "c:\tWindows\\...";
+
+        System.out.println( message2);
+        System.out.println(message);
     }
 
 }
