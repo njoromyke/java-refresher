e5ed746629bb74fea5002a4014cba0539c840b11
njoromyke <mykenjoroge01@gmail.com>
2023-04-15 16:00:40 +0300

    Arrays

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -1,17 +1,16 @@
 package com.mykesoftware;
 
-import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
 
-        String message = "Hello \"Mike\" "; // double quote
-        String message2 = "c:\tWindows\\...";
+        int[] numbers = {2,3,4,5,6,1};
+        Arrays.sort(numbers);
 
-        System.out.println( message2);
-        System.out.println(message);
+        System.out.println(Arrays.toString(numbers));
     }
 
 }
