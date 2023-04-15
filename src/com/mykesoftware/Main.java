80a6dd91450429e85b59e34e291b73d6b59955e8
njoromyke <mykenjoroge01@gmail.com>
2023-04-15 16:04:29 +0300

    multi-demensional arrays

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -7,10 +7,10 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbers = {2,3,4,5,6,1};
-        Arrays.sort(numbers);
+        int[][] numbers ={{1,2,3},{4,5,6}};
+        System.out.println(Arrays.deepToString(numbers));
 
-        System.out.println(Arrays.toString(numbers));
+
     }
 
 }
