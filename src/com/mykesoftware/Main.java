d306ed7daf2f3a4d812738e09934e7c053385601
njoromyke <mykenjoroge01@gmail.com>
2023-04-15 13:00:24 +0300

    Reference types

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -1,12 +1,13 @@
 package com.mykesoftware;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-     byte age =30;
-     long viewsCount = 3_123_456_789L;
-     float price = 10.99F;
-     char letter = 'M';
-     boolean isEligible = false;
+     byte age = 30;
 
+        Date date = new Date();
+
+        System.out.println(date);
     }
 }
