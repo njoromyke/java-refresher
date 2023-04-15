eb4754c3022c300472216436fe148d3770149688
njoromyke <mykenjoroge01@gmail.com>
2023-04-15 15:42:19 +0300

    Reference types

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -1,13 +1,14 @@
 package com.mykesoftware;
 
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-     byte age = 30;
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 3;
+        System.out.println(point2);
 
-        Date date = new Date();
-
-        System.out.println(date);
     }
 }
