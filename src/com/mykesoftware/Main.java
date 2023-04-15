dff9af0527a96d8d5b4e56468387b681f332c7f0
njoromyke <mykenjoroge01@gmail.com>
2023-04-15 12:31:12 +0300

    Primitive types

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -2,9 +2,11 @@ package com.mykesoftware;
 
 public class Main {
     public static void main(String[] args) {
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
+     byte age =30;
+     long viewsCount = 3_123_456_789L;
+     float price = 10.99F;
+     char letter = 'M';
+     boolean isEligible = false;
 
     }
 }
