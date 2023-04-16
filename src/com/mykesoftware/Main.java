0df2a85e38899e1248d22b9829443c54ec3cc8dd
njoromyke <mykenjoroge01@gmail.com>
2023-04-16 13:56:57 +0300

    Comparison operators

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -8,36 +8,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        System.out.print("Interest rate: ");
-        float interest = scanner.nextFloat();
-        float monthlyInterest = (interest / MONTHS_IN_YEAR) / PERCENT;
-
-
-        System.out.print("Period (Years): ");
-        byte period = scanner.nextByte();
-        int numberOfPayments = period * MONTHS_IN_YEAR;
-
-        double mortgage = principal * ((monthlyInterest * Math.pow((1+monthlyInterest),numberOfPayments)) / (Math.pow((1+ monthlyInterest),numberOfPayments)-1));
-
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String total = currency.format(mortgage);
-
-        System.out.println("Mortgage: " + total);
-
-
-
-
+        int x =1;
+        int y=1;
+        System.out.println(x>=y);
 
     }
-
-
-
 }
