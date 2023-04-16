c5bc4c1d93259cccd50d6624cd2127699a37f8ca
njoromyke <mykenjoroge01@gmail.com>
2023-04-16 14:01:50 +0300

    Logical operators

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -8,9 +8,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int x =1;
-        int y=1;
-        System.out.println(x>=y);
+        int temperature = 22;
+        boolean isWarm = temperature > 20 && temperature < 30;
+        System.out.println(isWarm);
+
 
     }
 }
