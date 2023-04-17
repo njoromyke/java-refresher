2e43982ed1ad70ca8b698683e6d077563cee7e2b
njoromyke <mykenjoroge01@gmail.com>
2023-04-17 09:23:47 +0300

    Ternary operator.

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
        int income = 120_0000;
-       boolean hasHighIncome = (income > 100_000);
-        System.out.println(hasHighIncome);
+       String className = (income > 100_000) ? "First" : "Economy";
+
     }
 }
