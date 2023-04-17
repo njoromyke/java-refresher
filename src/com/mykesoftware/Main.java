42dc1e3eb3095c6433f9dd2a4b0fe5fb64835af9
njoromyke <mykenjoroge01@gmail.com>
2023-04-17 15:33:38 +0300

    Creating objects.

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -6,7 +6,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        var textbox1 = new TextBox();
+        textbox1.setText("Box 1");
 
+        System.out.println(textbox1.text.toUpperCase());
+
+        var textBox2 = new TextBox();
+        textBox2.setText("New Text Box");
+        System.out.println(textBox2.text);
     }
 
 
--- src/com/mykesoftware/TextBox.java
+++ src/com/mykesoftware/TextBox.java
@@ -1,7 +1,7 @@
 package com.mykesoftware;
 
 public class TextBox {
-    public String text;
+    public String text ="";
     public  void setText(String text){
         this.text = text;
     }
