1cef7830bce3c01618cbcd332b5c10e05ab96824
njoromyke <mykenjoroge01@gmail.com>
2023-04-17 09:37:31 +0300

    while loop

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -8,9 +8,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for(int i= 1; i <= 5; i++){
-            System.out.println("Hello world" + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
 
+
+     while(!input.equals("quit")){
+         System.out.println("Input: ");
+         input = scanner.next().toLowerCase();
+         System.out.println(input);
+     }
     }
 }
