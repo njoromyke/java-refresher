3848fb425bc55acf9802880a768b60822fc056be
njoromyke <mykenjoroge01@gmail.com>
2023-04-17 16:27:20 +0300

    Getters and Setters

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -7,8 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 10;
+        employee.setBaseSalary(50_000);
+        employee.getBaseSalary();
+        employee.setHourlyRate(10);
         int wage = employee.calculateWage(10);
 
         System.out.println(wage);
