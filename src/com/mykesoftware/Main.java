d77f1bb526ac4d6f34698cd4a6cbf37846107a87
njoromyke <mykenjoroge01@gmail.com>
2023-04-17 09:08:25 +0300

    Logical operators

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -10,7 +10,13 @@ public class Main {
     public static void main(String[] args) {
         int temperature = 22;
         boolean isWarm = temperature > 20 && temperature < 30;
-        System.out.println(isWarm);
+        boolean hasHighIncome= true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = true;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+
+
+        System.out.println(isEligible);
 
 
     }
