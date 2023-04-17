3118fa3a1bc252648c05868bdc87f47ca5fc72dc
njoromyke <mykenjoroge01@gmail.com>
2023-04-17 15:28:00 +0300

    Creating a class

---
+++ src/com/mykesoftware/TextBox.java
@@ -0,0 +1,12 @@
+package com.mykesoftware;
+
+public class TextBox {
+    public String text;
+    public  void setText(String text){
+        this.text = text;
+    }
+
+    public  void clear(){
+        text = "";
+    }
+}
