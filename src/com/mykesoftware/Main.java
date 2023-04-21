9e16decf78c27ffa6c93eb136b3dfbbe38899706
njoromyke <mykenjoroge01@gmail.com>
2023-04-21 11:53:04 +0300

    Reducing Coupling

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -6,12 +6,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.setBaseSalary(50_000);
-        employee.setHourlyRate(10);
-        int wage = employee.calculateWage(10);
+        var browser = new Browser();
 
-        System.out.println(wage);
-
+        browser.navigate("127.0.0.1");
     }
 }
