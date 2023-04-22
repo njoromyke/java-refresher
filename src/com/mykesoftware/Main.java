df30c2d8fa260e103fc2cdda86b047a769538318
njoromyke <mykenjoroge01@gmail.com>
2023-04-22 22:49:44 +0300

    Object class

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -3,8 +3,8 @@ package com.mykesoftware;
 public class Main {
 
     public static void main(String[] args) {
-        var control = new TextBox();
-        control.disable();
-        System.out.println(control.isEnabled());
+        var box1 = new TextBox();
+        var box2 = box1;
+        System.out.println(box2.toString());
     }
 }
