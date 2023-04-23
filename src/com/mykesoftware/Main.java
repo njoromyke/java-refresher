519c5e2532b8368f8fb64ee568835adffff109f9
njoromyke <mykenjoroge01@gmail.com>
2023-04-23 12:29:45 +0300

    Comparing objects

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -3,17 +3,10 @@ package com.mykesoftware;
 public class Main {
 
     public static void main(String[] args) {
-       var control = new UIControl(true);
-       var textbox = new TextBox();
-       textbox.setText("Text box ");
-    show(control);
-    }
-
-    public  static void show(UIControl control){
-        if (control instanceof TextBox) {
-            var textBox = (TextBox) control;
-            textBox.setText("Hello box");
-        }
-        System.out.println(control);
+        var point1 = new Point(1,2);
+        var point2 = new
+                 Point(1,2);
+        System.out.println(point2.hashCode( ));
+        System.out.println(point1.hashCode( ));
     }
 }
