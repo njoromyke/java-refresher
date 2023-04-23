34118221663b97203dbf7e6c713860b11f67f6a8
njoromyke <mykenjoroge01@gmail.com>
2023-04-23 15:02:46 +0300

    Constructor injection

--- src/com/mykesoftware/Main.java
+++ src/com/mykesoftware/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         var calculator = new TaxCalculator2018(100);
-        var report = new TaxReport(calcluator);
+        var report = new TaxReport(calculator);
     }
 }
